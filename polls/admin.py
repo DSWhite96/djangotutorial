6f05b414a29329f9c7a3aae1d72933c11fc94417
Derrick White <derrickswhite@outlook.com>
2025-06-08 00:43:47 -0700

    Added test data to database (and fixed time zone)

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+#You need to import your models in order for them to be viewable by the admin
+from .models import Question
 
 # Register your models here.
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 """
 Three-step process for changing models in Django:
     1) Change your models in models.py
@@ -9,10 +12,29 @@ Three-step process for changing models in Django:
 # Create your models here.
 # Models = python representations of db tables
 class Question(models.Model):
+    def __str__(self):
+        """
+        Edits what is output when an instance is output to the terminal
+        """
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        Checks whether or not a question was published within the last day
+
+        Parameters:
+            None
+        Returns:
+            bool - Whether or not the question was published in the last day
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model):
+    def __str__(self): #Important to add these to models
+        return self.choice_text
     #Cascade = changes made to parent tables apply to child tables
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
